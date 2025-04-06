8b1e33655f694b322fac471f06ac4783dbcf9c1e
settseth <116912800+settseth@users.noreply.github.com>
2025-04-06 13:39:16 +0200

    Delete MMIPlanner_Front directory

--- MMIPlanner_Front/app/controllers/auth.controller.js
+++
@@ -1,5 +0,0 @@
-// user.controller.js
-angular.module('MMIPlanner')
-  .controller('UserController', function($scope) {
-    $scope.message = "Bienvenue dans la page de connexion!";
-  });
--- MMIPlanner_Front/app/controllers/event.controller.js
+++
@@ -1,3 +0,0 @@
-app.controller('EventController', function($scope) {
-    $scope.message = "Bienvenue sur la page des événements!";
-});
